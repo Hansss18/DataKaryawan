068adb7a8422536ae5ccecc694d23bcfe75a9a12
Hansss.18 <farhannamikaze18@gmail.com>
2023-02-17 09:52:06 +0700

    first commit

---
+++ react-frontend/src/components/parts/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer bg-dark">
+                <a href="#" style={{textDecoration: "none", color: "white"}}>
+                    <i className="fab fa-github fa-2x mr-3 mt-2" style={{color: "white"}}></i>
+                </a>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
